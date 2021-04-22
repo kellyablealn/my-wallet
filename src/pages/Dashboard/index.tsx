e44363f39295c348e8206f690229abc97c5bc232
Kelly Azevedo Borges Leal Neves <kellyableal@gmail.com>
2021-04-22 12:11:37 -0300

    Pie chart

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,6 +4,7 @@ import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
 import WalletBox from '../../components/WalletBox';
 import MessageBox from '../../components/MessageBox';
+import PieChart from '../../components/PieChart';
 
 import gains from '../../repositories/gains';
 import expenses from '../../repositories/expenses';
@@ -115,6 +116,30 @@ const Dashboard: React.FC = () => {
         }
     }, [totalBalance]);
 
+    const relationExpensesVersusGains = useMemo(() => {
+        const total = totalGains + totalExpenses;
+
+        const percentGains = (totalGains / total) * 100;
+        const percentExpenses = (totalExpenses / total) * 100;
+        
+        const data = [
+            {
+                name: 'Entradas',
+                value: totalGains,
+                percent: Number(percentGains.toFixed(1)),
+                color: '#E44C4E'
+            },
+            {
+                name: 'Saídas',
+                value: totalExpenses,
+                percent: Number(percentExpenses.toFixed(1)),
+                color: '#F7931B'
+            }
+        ];
+
+        return data;
+    }, [totalGains, totalExpenses]);
+
     const handleMonthSelected = (month: string) => {
         try {
             const parseMonth = Number(month);
@@ -172,6 +197,7 @@ const Dashboard: React.FC = () => {
                     description={message.description}
                     footerText={message.footerText}
                     icon={message.icon}/>
+                <PieChart data={relationExpensesVersusGains}/>    
             </Content>
         </Container>
     );
